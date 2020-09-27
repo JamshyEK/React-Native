4663cbad4ebaa883290e3a92bc5de18f15d3ded9
JamshyEK <ekmjamshy@gmail.com>
2020-09-27 23:46:18 +0530

    Components Part 1

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main/>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app! Jamshy EK</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#C0C0C0',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
